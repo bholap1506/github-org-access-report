3fe490a6b902e01f65a3e429eca55c232514cfc2
bholap1506 <158184287+bholap1506@users.noreply.github.com>
2026-03-14 17:33:38 +0530

    feat: Add Spring Boot main application class with caching enabled

---
+++ src/main/java/com/github/accessreport/GitHubAccessReportApplication.java
@@ -0,0 +1,15 @@
+package com.github.accessreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class GitHubAccessReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubAccessReportApplication.class, args);
+    }
+
+}
