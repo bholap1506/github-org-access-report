16d0051798cf5b3372b90f2f8aea5c4b1e8f4f8d
bholap1506 <158184287+bholap1506@users.noreply.github.com>
2026-03-15 22:00:35 +0530

    model: Add GitHubRepository entity class

---
+++ src/main/java/com/github/accessreport/model/d../entity/GitHubRepository.java
@@ -0,0 +1,68 @@
+package com.github.accessreport.model.entity;
+
+public class GitHubRepository {
+
+    private String name;
+    private String fullName;
+    private Boolean isPrivate;
+    private String defaultBranch;
+    private String description;
+    private String htmlUrl;
+
+    public GitHubRepository() {
+    }
+
+    public GitHubRepository(String name, String fullName, Boolean isPrivate) {
+        this.name = name;
+        this.fullName = fullName;
+        this.isPrivate = isPrivate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Boolean getIsPrivate() {
+        return isPrivate;
+    }
+
+    public void setIsPrivate(Boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public String getDefaultBranch() {
+        return defaultBranch;
+    }
+
+    public void setDefaultBranch(String defaultBranch) {
+        this.defaultBranch = defaultBranch;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
+}
